cef4fd62629d3d3c48ad37e1137f41a86b86c098
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-16 01:37:07 -0300

    now getKinglonCharHex tests check for an actual hexadecimal instead of a string

--- tests/kinglonTranslatorTest.js
+++ tests/kinglonTranslatorTest.js
@@ -12,18 +12,18 @@ describe('kinglonTranslator', () => {
     })
 
     it('getKinglonCharHex should return 0xF8E5 if \'u\' is passed', () => {
-        expect(kinglonTranslator.getKinglonCharHex('u')).to.equal('0xF8E5')
+        expect(kinglonTranslator.getKinglonCharHex('u')).to.equal(0xF8E5)
     })
 
     it('getKinglonCharHex should return 0xF8D6 if \'h\' is passed', () => {
-        expect(kinglonTranslator.getKinglonCharHex('h')).to.equal('0xF8D6')
+        expect(kinglonTranslator.getKinglonCharHex('h')).to.equal(0xF8D6)
     })
 
     it('getKinglonCharHex should return 0xF8E1 if \'r\' is passed', () => {
-        expect(kinglonTranslator.getKinglonCharHex('r')).to.equal('0xF8E1')
+        expect(kinglonTranslator.getKinglonCharHex('r')).to.equal(0xF8E1)
     })
 
     it('getKinglonCharHex should return 0xF8D0 if \'a\' is passed', () => {
-        expect(kinglonTranslator.getKinglonCharHex('a')).to.equal('0xF8D0')
+        expect(kinglonTranslator.getKinglonCharHex('a')).to.equal(0xF8D0)
     })
 })
