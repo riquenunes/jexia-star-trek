bbdceca0e64bfc3f76d33eb62b4d8e1451096556
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-15 22:10:29 -0300

    preliminary implementation of isTextTranslatable

--- kinglonTranslator.js
+++ kinglonTranslator.js
@@ -1,6 +1,9 @@
 module.exports = {
+    // checks if the input string contains only characters of the pIqaD​ alphabet
     isTextTranslatable(text) {
+        const regex = /(a|b|ch|d|e|gh|h|i|j|l|m|n|o|p|q|r|s|t|u|v|w|y|’|\s|\d)+/ig
 
+        return regex.test(text)
     },
     getKinglonCharHex(latinChar) {
 
