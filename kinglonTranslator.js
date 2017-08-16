459f2fccdf81fe1c39703dc176f456bab311891b
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-16 01:37:45 -0300

    function getKinglonCharHex implemented

--- kinglonTranslator.js
+++ kinglonTranslator.js
@@ -1,11 +1,22 @@
+const klingonAlphabet =  ['a','b','ch','d','e','gh','h','i','j','l','m','n','ng','o','p','q','q','r','s','t','tlh','u','v','w','y','’']
+
 module.exports = {
     // checks if the input string contains only characters of the pIqaD​ alphabet
     isTextTranslatable(text) {
         const regex = /(a|b|ch|d|e|gh|h|i|j|l|m|n|o|p|q|r|s|t|u|v|w|y|’|\s|\d)+/ig
-
         return regex.test(text)
     },
-    getKinglonCharHex(latinChar) {
+    getKinglonCharHex(token) {
+        if (!isNaN(token)) {
+            return 0xF8F + parseInt(token)
+        } else if (token === ' ') {
+            return 0x0020
+        } else {
+            const tokenIndex = klingonAlphabet.indexOf(token)
 
+            if (tokenIndex > -1) {
+                return 0xF8D0 + tokenIndex
+            }
+        }
     }
 }
