ec352031adcd81496b3a49c98bf896a7a751d90b
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-15 21:20:34 -0300

    added some more test cases

--- tests/kinglonTranslatorTest.js
+++ tests/kinglonTranslatorTest.js
@@ -7,7 +7,19 @@ describe('kinglonTranslator', () => {
         expect(kinglonTranslator.isTextTranslatable('')).to.equal(false)
     })
 
-    it('getKinglonCharHex should return 0xF8E5 if \'U\' is passed', () => {
-        expect(kinglonTranslator.getKinglonCharHex('U')).to.equal('0xF8E5')
+    it('getKinglonCharHex should return 0xF8E5 if \'u\' is passed', () => {
+        expect(kinglonTranslator.getKinglonCharHex('u')).to.equal('0xF8E5')
+    })
+
+    it('getKinglonCharHex should return 0xF8D6 if \'h\' is passed', () => {
+        expect(kinglonTranslator.getKinglonCharHex('h')).to.equal('0xF8D6')
+    })
+
+    it('getKinglonCharHex should return 0xF8E1 if \'r\' is passed', () => {
+        expect(kinglonTranslator.getKinglonCharHex('r')).to.equal('0xF8E1')
+    })
+
+    it('getKinglonCharHex should return 0xF8D0 if \'a\' is passed', () => {
+        expect(kinglonTranslator.getKinglonCharHex('a')).to.equal('0xF8D0')
     })
 })
