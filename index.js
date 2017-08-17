020e607887cc06a0307261174334acbc514b50e9
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-17 04:20:52 -0300

    program should now finally print the input character's species

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const klingonTranslator = require('./klingonTranslator')
+const stapi = require('./stapi')
 const inputText = process.argv.splice(2).join(' ')
 
 if (klingonTranslator.isTextTranslatable(inputText)) {
     console.log(klingonTranslator.translateText(inputText))
+
+    stapi.searchCharactersByName(inputText).then(characters => {
+        return stapi.getCharacterDetails(characters[0].uid)
+    }).then(character => {
+        console.log(character.characterSpecies[0].name)
+    })
 } else {
     console.log('input text is not translatable')
 }
