237a6a36f358e532e62186b7b3fd51ee2891e6ab
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-18 02:53:23 -0300

    program now shows a message whenever a character is not found on stapi and should now search for the character even know its name may not me translated (looking at you Spock and Kirk)

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const inputText = process.argv.splice(2).join(' ')
 
 if (klingonTranslator.isTextTranslatable(inputText)) {
   console.log(klingonTranslator.translateText(inputText))
-
-  stapi.searchCharactersByName(inputText).then(characters => {
-    return stapi.getCharacterDetails(characters[0].uid)
-  }).then(character => {
-    console.log(character.characterSpecies[0].name)
-  })
 } else {
   console.log('input text is not translatable')
 }
+
+stapi.searchCharactersByName(inputText).then(characters => {
+  if (characters && characters.length) {
+    return stapi.getCharacterDetails(characters[0].uid)
+  }
+
+  return Promise.reject('no matching character was found')
+}).then(character => {
+  console.log(character.characterSpecies[0].name)
+}).catch(err => {
+  console.log(err)
+})
