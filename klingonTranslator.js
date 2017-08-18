1dc4f6695ba1100b9661335635d48a4fc5bdd4e6
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-18 04:18:18 -0300

    more comments added

--- klingonTranslator.js
+++ klingonTranslator.js
@@ -8,12 +8,22 @@ module.exports = {
     const testText = text.replace(' ', '')
     const matches = testText.match(klingonAlphabetRegex)
 
+    // test if there were matches in the input string and if the number
+    // of matched characters is the same as the number of characters in
+    // the input string. If this number is different it means that at
+    // least one character is not part of the pIqaD alphabet and the
+    // text is not translatable
     return !!matches && matches.join('').length === testText.length
   },
+  // return a number that represents the given letter
   getKlingonCharHex(token) {
     if (!isNaN(token)) {
+      // if the input token is a number we just take it and sum with
+      // 0xF8F0 (address of number 0)
       return 0xF8F0 + parseInt(token)
     } else {
+      // otherwise we take the letter position on the alphabet and sum
+      // with 0xF8D0 (address of the first letter)
       const tokenIndex = klingonAlphabet.indexOf(token)
 
       if (tokenIndex > -1) {
@@ -21,10 +31,13 @@ module.exports = {
       }
     }
   },
+  // returns a string containing the ouput to be printed on the screen
   translateText(text) {
     let translatedText = ''
     const words = text.trim().split(' ')
 
+    // loop through each word so that we can add an space separator
+    // between each one
     words.forEach((word, index) => {
       if (index > 0) {
         translatedText += ' 0x0020'
