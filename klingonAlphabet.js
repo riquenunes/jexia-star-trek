a8adf639994b0f37eb3f3eeb601465f3198c4f88
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-18 04:00:47 -0300

    regex is now case insensitive

--- klingonAlphabet.js
+++ klingonAlphabet.js
@@ -1,7 +1,7 @@
 const alphabet = ['a', 'b', 'ch', 'd', 'e', 'gh', 'h', 'i', 'j', 'l', 'm', 'n', 'ng', 'o', 'p', 'q', 'q', 'r', 's', 't', 'tlh', 'u', 'v', 'w', 'y', '’']
 // here we make a RegExp with the klingonAlphabet array reversed, so that 'ng'
 // can be matched instead of 'n', for example
-const regex = new RegExp(alphabet.slice().reverse().join('|') + '|\\d', 'g')
+const regex = new RegExp(alphabet.slice().reverse().join('|') + '|\\d', 'ig')
 
 module.exports = {
   alphabet,
