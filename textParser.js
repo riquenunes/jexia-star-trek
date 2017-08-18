618c499920a4506d5c45eb6b34fdbe0f915cb419
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-18 02:49:16 -0300

    changed textParser to use the alphabet's regex from klingonAlphabet module instead

--- textParser.js
+++ textParser.js
@@ -1,14 +1,11 @@
-const klingonAlphabet = require('./klingonAlphabet')
-
-// here we make a RegExp with the klingonAlphabet array reversed, so that 'ng'
-// can be matched instead of 'n', for example
-const regex = new RegExp(klingonAlphabet.slice().reverse().join('|'), 'g')
+const klingonAlphabet = require('./klingonAlphabet').alphabet
+const klingonAlphabetRegex = require('./klingonAlphabet').regex
 
 module.exports = {
   // returns an array with a list of pIqaD letters represented by the Roman alphabet
   // basically we do this so we can know the difference between 't' and 'tlh', for example
   // as both are different letters in Klingon
   getTokenList(text) {
-    return text.toLowerCase().match(regex)
+    return text.toLowerCase().match(klingonAlphabetRegex)
   }
 }
