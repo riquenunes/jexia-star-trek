c3205d555dddc6c55781a0847d8a39e7298fb628
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-18 02:04:08 -0300

    unit tests reorganized so that their results can be viewed more easily

--- tests/klingonTranslatorTest.js
+++ tests/klingonTranslatorTest.js
@@ -3,35 +3,41 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe('klingonTranslator', () => {
-    it('isTextTranslatable should return false if an untranslatable text is passed', () => {
-        expect(klingonTranslator.isTextTranslatable('kzc')).to.equal(false)
+    describe('#isTextTranslatable()', () => {
+        it('should return false if an untranslatable text is passed', () => {
+            expect(klingonTranslator.isTextTranslatable('kzc')).to.equal(false)
+        })
+
+        it('should return true if \'Uhura\' is passed', () => {
+            expect(klingonTranslator.isTextTranslatable('Uhura')).to.equal(true)
+        })
     })
 
-    it('isTextTranslatable should return true if \'Uhura\' is passed', () => {
-        expect(klingonTranslator.isTextTranslatable('Uhura')).to.equal(true)
+    describe('#getklingonCharHex()', () => {
+        it('should return 0xF8E5 if \'u\' is passed', () => {
+            expect(klingonTranslator.getKlingonCharHex('u')).to.equal(0xF8E5)
+        })
+
+        it('should return 0xF8D6 if \'h\' is passed', () => {
+            expect(klingonTranslator.getKlingonCharHex('h')).to.equal(0xF8D6)
+        })
+
+        it('should return 0xF8E1 if \'r\' is passed', () => {
+            expect(klingonTranslator.getKlingonCharHex('r')).to.equal(0xF8E1)
+        })
+
+        it('should return 0xF8D0 if \'a\' is passed', () => {
+            expect(klingonTranslator.getKlingonCharHex('a')).to.equal(0xF8D0)
+        })
     })
 
-    it('getklingonCharHex should return 0xF8E5 if \'u\' is passed', () => {
-        expect(klingonTranslator.getKlingonCharHex('u')).to.equal(0xF8E5)
-    })
+    describe('#translateText()', () => {
+        it('should return \'0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0\' if \'Uhura\' is passed', () => {
+            expect(klingonTranslator.translateText('Uhura')).to.equal('0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0')
+        })
 
-    it('getklingonCharHex should return 0xF8D6 if \'h\' is passed', () => {
-        expect(klingonTranslator.getKlingonCharHex('h')).to.equal(0xF8D6)
-    })
-
-    it('getklingonCharHex should return 0xF8E1 if \'r\' is passed', () => {
-        expect(klingonTranslator.getKlingonCharHex('r')).to.equal(0xF8E1)
-    })
-
-    it('getklingonCharHex should return 0xF8D0 if \'a\' is passed', () => {
-        expect(klingonTranslator.getKlingonCharHex('a')).to.equal(0xF8D0)
-    })
-
-    it('translateText(\'Uhura\') should return \'0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0\'', () => {
-        expect(klingonTranslator.translateText('Uhura')).to.equal('0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0')
-    })
-
-    it('translateText(\'Uhura Nunes\') should return \'0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0 0x0020 0xF8DB 0xF8E5 0xF8DB 0xF8D4 0xF8E2\'', () => {
-        expect(klingonTranslator.translateText('Uhura Nunes')).to.equal('0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0 0x0020 0xF8DB 0xF8E5 0xF8DB 0xF8D4 0xF8E2')
+        it('should return \'0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0 0x0020 0xF8DB 0xF8E5 0xF8DB 0xF8D4 0xF8E2\' if \'Uhura Nunes\'', () => {
+            expect(klingonTranslator.translateText('Uhura Nunes')).to.equal('0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0 0x0020 0xF8DB 0xF8E5 0xF8DB 0xF8D4 0xF8E2')
+        })
     })
 })
--- tests/textParserTest.js
+++ tests/textParserTest.js
@@ -3,11 +3,13 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe('textParser', () => {
-  it('getTokenList(\'Uhura\') should return [\'u\', \'h\', \'u\', \'r\', \'a\']', () => {
-    expect(textParser.getTokenList('Uhura')).to.deep.equal(['u','h','u','r','a'])
-  })
+  describe('#getTokenList()', () => {
+    it('should return [\'u\', \'h\', \'u\', \'r\', \'a\'] if \'Uhura\' is passed', () => {
+      expect(textParser.getTokenList('Uhura')).to.deep.equal(['u','h','u','r','a'])
+    })
 
-  it('getTokenList(\'Uchughang\') should return [\'u\', \'ch\', \'u\', \'gh\', \'a\', \'ng\']', () => {
-    expect(textParser.getTokenList('Uchughang')).to.deep.equal(['u', 'ch', 'u', 'gh', 'a', 'ng'])
+    it('should return [\'u\', \'ch\', \'u\', \'gh\', \'a\', \'ng\'] if \'Uchughang\' is passed', () => {
+      expect(textParser.getTokenList('Uchughang')).to.deep.equal(['u', 'ch', 'u', 'gh', 'a', 'ng'])
+    })
   })
 })
