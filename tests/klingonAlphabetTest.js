f038b609f7295c3fbd8c5dc6b09565b4ce368169
Henrique Nunes <henrique_nunes@hotmail.com.br>
2017-08-18 02:48:05 -0300

    added some regex test cases

--- tests/klingonAlphabetTest.js
+++ tests/klingonAlphabetTest.js
@@ -1,9 +1,28 @@
-const klingonAlphabet = require('../klingonAlphabet')
+const klingonAlphabet = require('../klingonAlphabet').alphabet
+const klingonAlphabetRegex = require('../klingonAlphabet').regex
 const chai = require('chai')
 const expect = chai.expect
 
 describe('klingonAlphabet', () => {
-  it('should be an array with the klingonAlphabet', () => {
-    expect(klingonAlphabet).to.deep.equal(['a', 'b', 'ch', 'd', 'e', 'gh', 'h', 'i', 'j', 'l', 'm', 'n', 'ng', 'o', 'p', 'q', 'q', 'r', 's', 't', 'tlh', 'u', 'v', 'w', 'y', '’'])
+  describe('#alphabet', () => {
+    it('should be an array with the klingonAlphabet', () => {
+      expect(klingonAlphabet).to.deep.equal(['a', 'b', 'ch', 'd', 'e', 'gh', 'h', 'i', 'j', 'l', 'm', 'n', 'ng', 'o', 'p', 'q', 'q', 'r', 's', 't', 'tlh', 'u', 'v', 'w', 'y', '’'])
+    })
+  })
+
+  describe('#regex', () => {
+    it('should match every character in the alphabet', () => {
+      const klingonAlphabetString = klingonAlphabet.join('')
+
+      expect(klingonAlphabetString.length).to.equal(klingonAlphabetString.match(klingonAlphabetRegex).join('').length)
+    })
+
+    it('should not match unexisting characters in the Klingon Alphabet', () => {
+      expect(klingonAlphabetRegex.test('cfkxz')).to.equal(false)
+    })
+
+    it('should match any number', () => {
+      expect('123456789'.match(klingonAlphabetRegex).length).to.equal(9)
+    })
   })
 })
